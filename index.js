02be9006bf45375202ec2e68fd05b7384d20c108
velpulakavitha <kavitha.v@medvol.in>
2025-02-04 20:06:57 +0530

    new db changes

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { Pool } = require('pg');
 const serverless = require('serverless-http');
 const app = express();
 const cors = require("cors");
@@ -7,14 +6,7 @@ const path = require("path");
 const port = 3001;
 const save_user_login = require('./router/user_login_router');
 const helmet = require("helmet");
-
-
-// Replace the URL with your ElephantSQL database URL
-const connectionString = 'postgres://imfhhxyp:1gBfqjymgT1l1pHd8roawKbljUrAMmtn@tiny.db.elephantsql.com:5432/imfhhxyp';
-
-const pool = new Pool({
-  connectionString: connectionString,
-});
+const pool=require('./db')
 
 const corsOptions = {
   origin: '*',
@@ -67,7 +59,32 @@ app.get('/test', async (req, res) => {
 app.get('/', async (req, res) => {
   try {
 
-    res.json("hello");
+    let reslut;
+ reslut=await pool.query('select * from user_login_hdr order by 1 desc')
+
+ 
+//     await pool.query(`CREATE TABLE user_login_hdr (
+//   id SERIAL PRIMARY KEY,      -- Auto-incrementing unique ID for each user
+//   name VARCHAR(100) NOT NULL, -- Name column, cannot be null
+//   email VARCHAR(100) NOT NULL UNIQUE, -- Email column, unique constraint to prevent duplicates
+//   phone VARCHAR(15),          -- Phone number column
+//   password VARCHAR(255) NOT NULL -- Password column, should be hashed and salted in practice
+// );
+// `)
+
+//  reslut=await pool.query(`CREATE TABLE study_form_hdr (
+//   id SERIAL PRIMARY KEY,  -- Auto-incrementing ID for each row
+//   name VARCHAR(100) NOT NULL,
+//   email VARCHAR(100) NOT NULL,
+//   readinformation JSONB[],  -- This column stores an array of JSONB objects
+//   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+// );
+// `)
+// pool.query(`ALTER TABLE study_form_hdr
+// ADD COLUMN createddate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+// ADD COLUMN modifieddate TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+// `)
+    res.json(reslut?.rows||'hello');
   } catch (error) {
     console.error('Error executing SQL query', error);
     res.status(500).json({ error: 'Internal Server Error' });
